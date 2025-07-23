e60cef6ac6ca4eb059ec5acf104c4fc402ca3a95
Saurabh <saurabhkumarmehto@gmail.com>
2025-07-23 09:09:16 +0530

    convert class to function

--- services/cronService.js
+++ services/cronService.js
@@ -2,18 +2,17 @@ const cron = require("node-cron")
 const { CurrentCoin, HistoryCoin } = require("../models/Coin")
 const coinGeckoService = require("./coinGeckoService")
 
-class CronService {
-  constructor() {
-    this.isRunning = false
-  }
+// Use a closure to maintain internal state like isRunning
+function createCronService() {
+  let isRunning = false
 
-  async updateCoinData() {
-    if (this.isRunning) {
+  async function updateCoinData() {
+    if (isRunning) {
       console.log("Cron job already running, skipping...")
       return
     }
 
-    this.isRunning = true
+    isRunning = true
     console.log("Starting scheduled coin data update...")
 
     try {
@@ -23,7 +22,7 @@ class CronService {
 
       // Update current data (upsert)
       const currentUpdatePromises = coinData.map((coin) =>
-        CurrentCoin.findOneAndUpdate({ coinId: coin.coinId }, coin, { upsert: true, new: true }),
+        CurrentCoin.findOneAndUpdate({ coinId: coin.coinId }, coin, { upsert: true, new: true })
       )
 
       await Promise.all(currentUpdatePromises)
@@ -52,36 +51,41 @@ class CronService {
     } catch (error) {
       console.error("Error in cron job:", error.message)
     } finally {
-      this.isRunning = false
+      isRunning = false
       console.log("Cron job completed")
     }
   }
 
-  startCronJob() {
-    // Run every hour at minute 0
+  function startCronJob() {
+    // Schedule to run every hour at minute 0
     cron.schedule(
       "0 * * * *",
       () => {
-        this.updateCoinData()
+        updateCoinData()
       },
       {
         scheduled: true,
         timezone: "UTC",
-      },
+      }
     )
 
     console.log("Cron job scheduled to run every hour")
 
     // Run immediately on startup
     setTimeout(() => {
-      this.updateCoinData()
-    }, 5000) // Wait 5 seconds after startup
+      updateCoinData()
+    }, 5000) // 5-second delay after startup
+  }
+
+  return {
+    startCronJob,
+    updateCoinData,
   }
 }
 
-const cronService = new CronService()
+const cronService = createCronService()
 
 module.exports = {
-  startCronJob: () => cronService.startCronJob(),
-  updateCoinData: () => cronService.updateCoinData(),
+  startCronJob: cronService.startCronJob,
+  updateCoinData: cronService.updateCoinData,
 }
